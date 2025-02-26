e1f5542264f208573a0591960056d357b810252c
katlehomadaba <katlehomadaba44@gmail.com>
2025-02-27 00:45:45 +0200

    added basic content on about page

---
+++ typescriptAnt/src/pages/about/about.tsx
@@ -0,0 +1,18 @@
+About Page
+import { Typography,Space } from 'antd';
+const { Title, Paragraph } = Typography;
+const AboutPage = () => {
+  return (
+    <Space direction="vertical" size="large" style={{ width: '100%' }}>
+      <Typography>
+        <Title level={2}>The about Overview</Title>
+        <Paragraph>
+          Welcome to your adminPage. Here's what's happening today.
+        </Paragraph>
+      </Typography>
+
+    </Space>
+  );
+};
+
+export default AboutPage;
