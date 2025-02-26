e613181091409d89273f86f81767dcb8c98d0da8
katlehomadaba <katlehomadaba44@gmail.com>
2025-02-27 01:05:09 +0200

    public routes added

--- typescriptAnt/src/App.tsx
+++ typescriptAnt/src/App.tsx
@@ -1,10 +1,23 @@
 import { Route,Routes } from "react-router"
 import Login from "./pages/login/login"
+import ClientLayout from "./layouts/client"
+import AdminLayout from "./layouts/admin"
+import EmptyLayout from "./layouts/empty"
+import AboutPage from "./pages/about/about"
+import HomePage from "./pages/home/home"
 function App() {
   return (
-    <>
-    <Login/>
-    </>
+    <Routes>
+      {/* Public Routes */}
+      {/* The public routes are netsted inside the EmptyLayout Route  */}
+      <Route path="/" element={<EmptyLayout/>}>
+      {/* I dont think i understand why there is two routes for login */}
+        <Route index element={<Login/>}/>
+        <Route path="login" element={<Login/>}/>
+      </Route>
+      {/* Protected Admin Routes */}
+      {/* Protect Client Routes */}
+    </Routes>
   )
 }
 
